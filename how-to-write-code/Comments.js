d21882bfbf8b6713cb13d5e3092fdd4bbe870612
dustinmmiller <d0nut01@hotmail.com>
2024-10-16 18:45:39 -0500

    tabs and new lines added to code

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,12 +1,11 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This does not print.");
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
-   /* Here is how
-   to have
-   multi-line
-   comments. */
+// This is a single line comment
+/* This is a multi-line 
+comment */
 
    console.log("Comments make your code more readable by others.");
