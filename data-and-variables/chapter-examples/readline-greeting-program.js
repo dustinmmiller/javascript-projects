35e6c39e34630b083d9644d6bb8e06e4ba46d38d
dustinmmiller <d0nut01@hotmail.com>
2024-10-22 18:32:18 -0500

    changed message to ask for and print first and last name

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,5 @@
 const input = require('readline-sync');
 
-let name = input.question("Enter your name: ");
+let name = input.question("Enter your first name: ");
+let otherName = input.question("Enter your last name: ");
+console.log("First name: " + name + "\nLast name: " + otherName + "\nLast, First: " + otherName + ",", name);
