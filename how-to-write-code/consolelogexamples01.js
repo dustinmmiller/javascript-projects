b31d653f1a5525f3c6ed40dda511824e47c59174
dustinmmiller <d0nut01@hotmail.com>
2024-10-24 12:19:40 -0500

    went through examples here

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -2,5 +2,5 @@ console.log('Hello, JavaScript.');
 console.log(2001);
 console.log("What","do", "\ncommas","do?");
 console.log("Does", "adding",      "space", "matter?");
-console.log('Launch' + '\tCode');
+console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
