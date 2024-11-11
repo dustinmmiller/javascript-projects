ca2ad2c993849ffec67043ed08b5589ede7aea39
dustinmmiller <d0nut01@hotmail.com>
2024-11-11 14:55:10 -0600

    got rid of a semicolon after a while loop

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -18,7 +18,7 @@ let num1 = Number(input.question("Please select a cabinet in the cargoHold numbe
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 while (num1 !== 0 && num1 !== 1 && num1 !== 2 && num1 !== 3) {
     num1 = Number(input.question("Please select a cabinet in the cargoHold numbered 0 - 3: "));
-};
+}
 console.log(`The contents of cabinet ${num1} are: ${cargoHold[num1]}`);
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
