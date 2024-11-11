72a229d1de968565a66fdd439772a4c892e131e8
dustinmmiller <d0nut01@hotmail.com>
2024-11-11 12:42:47 -0600

    made a while loop so you have to select a cabinet

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -16,15 +16,15 @@ console.log(cargoHold);
 const input = require('readline-sync');
 let num1 = Number(input.question("Please select a cabinet in the cargoHold numbered 0 - 3: "));
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-if (num1 !== 0 && num1 !== 1 && num1 !== 2 && num1 !== 3) {
-    console.log("Error: please select a cabinet: 0, 1, 2, or 3.");
-} else {
-    console.log(`The contents of cabinet ${num1} are: ${cargoHold[num1]}`);
-}
+while (num1 !== 0 && num1 !== 1 && num1 !== 2 && num1 !== 3) {
+    num1 = Number(input.question("Please select a cabinet in the cargoHold numbered 0 - 3: "));
+};
+console.log(`The contents of cabinet ${num1} are: ${cargoHold[num1]}`);
+
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
 let contents = input.question(`Please select an item from cabinet ${num1}: ` );
 let doesItContain = "DOES NOT";
 if (cargoHold[num1].includes(contents)) {
     doesItContain = "DOES";
 }
-console.log(`Cabinet ${num1} ${doesItContain} contain ${contents}."`);
+console.log(`Cabinet ${num1} ${doesItContain} contain ${contents}.`);
