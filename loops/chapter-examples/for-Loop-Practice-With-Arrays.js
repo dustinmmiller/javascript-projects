2d04727c5b6bd6d32ae1fd33f78568e7534c9bbe
dustinmmiller <d0nut01@hotmail.com>
2024-11-11 15:56:12 -0600

    completed the chapter examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+const family = ["Mom", "Dad", "brother", "sister", "wife"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < family.length; i++) {
+    console.log(family[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,7 @@
 // Create a string variable containing your name.
-
+const name = "Dustin";
 
 // Write a for loop that prints each character in your name on a different line.
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+}
