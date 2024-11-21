44d27daa6f2bc0adad1671a73abe015584dd23f5
dustinmmiller <d0nut01@hotmail.com>
2024-11-21 08:09:10 -0600

    wrote validators for user input

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,20 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
+let isValidStr = function(startsA) {
+  if (startsA[0] !== 'a') {
+    return false;
+  }
 
+  return true;
+}
 // TODO 2: write a validator 
 // that ensures input is a vowel
-
+let isValidVowel = function(vowel) {
+  if (vowel === 'a' || vowel === 'e' || vowel === 'i' || vowel === 'o' || vowel === 'u' || vowel === 'y') {
+    return true;
+  }
+  return false;
+}
 // Be sure to test your code!
+console.log(getValidInput('Enter a vowel:', isValidVowel));
