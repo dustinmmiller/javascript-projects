75b08e44c842087f38860c245ddfb6ff6020a763
dustinmmiller <d0nut01@hotmail.com>
2024-11-21 11:23:13 -0600

    made validations for a factorial recursion function and entered base case for recursion functions

--- more-on-functions/chapter-examples/recursion-base-case/factorial.js
+++ more-on-functions/chapter-examples/recursion-base-case/factorial.js
@@ -1,13 +1,43 @@
 //The following concept check assumes that only positive integers are passed to the function.
 
-function factorial(integer){
-  if (/*enter base case test here*/){
+function factorial(integer) {
+  if (integer === 1){
     return integer;
   } else {
     return integer*(factorial(integer-1));
   }
 }
 
-console.log(factorial(4));
+function isValidInput (integer) {
+  if (isValidStr(integer) === true || isValidDecimal(integer) === true || isValidNegative(integer) === true) {
+    return "Error please enter a positive whole number that is not a string!";
+  }
+  return factorial(integer);
+}
+
+let isValidStr = function(str) { 
+  if (typeof str === 'string') {
+    console.log(`${str} is a string. `);
+    return true;
+  }
+  return false;
+};
+let isValidDecimal = function(decimal) {
+  if (Number.isInteger(decimal) === false) {
+    console.log(`${decimal} is a decimal. `);
+    return true;
+  }
+  return false;
+};
+
+let isValidNegative = function(negative) {
+  if (negative <= 0) {
+    console.log(`${negative} is a number equal to or less than 0). `);
+    return true;
+  } 
+  return false
+};
+console.log(factorial(1));
+console.log(isValidInput(0));
 
 //Skill boost! Add validation to return an error message if the function is passed a string, negative number or a decimal value.
