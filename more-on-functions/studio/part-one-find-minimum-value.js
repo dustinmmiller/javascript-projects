17f39fc906758634b6a47567a1f6e8a89c5a7858
dustinmmiller <d0nut01@hotmail.com>
2024-11-25 23:26:26 -0600

    created a function to find minimum value and another to sort arrays rewrote a function to a recursive funtion and worked with the easy way to sort numbers

--- more-on-functions/studio/part-one-find-minimum-value.js
+++ more-on-functions/studio/part-one-find-minimum-value.js
@@ -1,5 +1,27 @@
 //1) Create a function with an array of numbers as its parameter. The function should iterate through the array and return the minimum value from the array. Hint: Use what you know about if statements to identify and store the smallest value within the array.
-
+/*let sortNum = function (arr) {
+    let minimumArr = [];
+    while (arr.length > 0) {
+        let minimumValue = arr[0];
+        for (let i = 0; i < arr.length; i++) {
+            if (minimumValue >= arr[i]) {
+             minimumValue = arr[i];
+            }
+        }
+        minimumArr.push(minimumValue);
+        arr.splice((arr.indexOf(minimumValue)), 1);
+    }
+    return minimumArr;
+};*/
+function sortNum(arr) {
+    let minimumArr = arr[0];
+    for(let i = 0; i < arr.length; i++){
+        if (minimumArr > arr[i]){
+            minimumArr = arr[i];
+        }
+    }
+    return minimumArr;
+}
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
@@ -7,4 +29,4 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Using one of the test arrays as the argument, call your function inside the console.log statement below.
 
-console.log(/* your code here */);
+console.log(sortNum(nums2));
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -19,11 +19,30 @@ function findMinValue(arr){
 6) Be sure to print the results in order to verify your code.*/
 
 //Your function here...
-
+/*function sortArr(arr){
+  let sortedArr = [];
+  while (arr.length > 0) {
+    sortedArr.push(findMinValue(arr));
+    arr.splice((arr.indexOf(findMinValue(arr))), 1);
+  }
+  return sortedArr;
+}*/
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
+function sortArr(arr){
+  if(arr.length <= 1){
+    return arr;
+  } else {
+    let sortedArr = [];
+    let temp = findMinValue(arr);
+    sortedArr.push(temp);
+    arr.splice(arr.indexOf(temp), 1);
+    return sortedArr.concat(sortArr(arr));
+  }
+}
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(sortArr(nums3));
