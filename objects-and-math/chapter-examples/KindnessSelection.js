018151981f32e1b5b3f24a97f0fcb064310f37a0
dustinmmiller <d0nut01@hotmail.com>
2024-11-27 07:17:07 -0600

    used randomSelection funciton and created random arrays and used for in loops for objects

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -7,10 +7,12 @@ function randomSelection(arr){
  
  let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
  
- for (i=0; i < 8; i++){
-   console.log(randomSelection(happiness));
+ let luckyArr = randomSelection([happiness, words]);
+ for (i=0; i < 2; i++){
+   console.log(randomSelection(luckyArr));
  }
- 
+ let arrC = [randomSelection(happiness), randomSelection(words)];
+ console.log(arrC);
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
  //b) Have the code randomly pick one array, and then print 2 random items from it.
