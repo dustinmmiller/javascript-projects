ef0a2c510a68bb6a110fbfd391a8b6357663e54d
dustinmmiller <d0nut01@hotmail.com>
2024-12-06 01:17:47 -0600

    used TDD by creating tests and an objet with a function that uses other created functions to deliver messages

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,58 @@
-
-let launchcode = {
-  
+const dividesByTwo = {
+    codeWord: 'Launch',
+    divides: function(num) {
+        if(num %2 === 0){
+            return true;
+        }
+        return false;
+    }
 }
 
+const dividesByThree = {
+    codeWord: 'Code',
+    divides: function(num) {
+        if(num %3 === 0){
+            return true;
+        }
+        return false;
+    }
+}
+
+const dividesByFive = {
+    codeWord: 'Rocks',
+    divides: function(num) {
+        if(num %5 === 0){
+            return true;
+        }
+        return false;
+    }
+}
+
+let launchcode = {
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(num) {
+        if(dividesByTwo.divides(num)) {
+            if(dividesByThree.divides(num)){
+                return dividesByTwo.codeWord + dividesByThree.codeWord + '!';
+            } else if (dividesByFive.divides(num)){
+                return dividesByTwo.codeWord + ' ' +  dividesByFive.codeWord + '! (CRASH!!!!)';
+            } else {
+            return dividesByTwo.codeWord + '!';
+            }
+        }
+        if(dividesByThree.divides(num)) {
+            return dividesByThree.codeWord + '!';
+        }
+        if(dividesByFive.divides(num)) {
+            return dividesByFive.codeWord + '!';
+        }   
+        return '';
+    }
+}
+
+console.log(launchcode.executiveDirector);
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,53 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("launchcode object has property.organization: 'nonprofit'", function() {
+    let result = launchcode;
+    expect(result.organization).toEqual('nonprofit');
+  });
+
+  test("launchcode object has executiveDirector: 'Jeff'", function(){
+    let result = launchcode;
+    expect(result.executiveDirector).toEqual('Jeff');
+  });
+
+  test("launchcode object has percentageCoolEmployees: 100", function(){
+    let result = launchcode;
+    expect(result.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("launchcode object has programsOffered: ['Web Development', 'Data Analysis', 'Liftoff']", function() { 
+    let result = launchcode;
+    expect(result.programsOffered).toContain("Web Development");
+    expect(result.programsOffered).toContain("Data Analysis");
+    expect(result.programsOffered).toContain("Liftoff");
+    expect(result.programsOffered.length).toEqual(3);
+  });
+
+  test("when passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch!'", function(){
+    let result = launchcode;
+    expect(result.launchOutput(4)).toEqual('Launch!');
+  });
+
+  test("when passed a nubmer divisble by 3 launchOutput() returns 'Code!'", function() {
+    let result = launchcode;
+    expect(result.launchOutput(9)).toEqual('Code!');
+  });
+
+  test("when passed a nubmer divisble by 5 launchOutput() returns 'Code!'", function() {
+    let result = launchcode;
+    expect(result.launchOutput(5)).toEqual('Rocks!');
+  });
+
+  test("when passed a number that is divisible by 2 AND 3, launchOutput() returns 'LaunchCode!'", function() {
+    let result = launchcode;
+    expect(result.launchOutput(6)).toEqual('LaunchCode!');
+  });
+
+  test("when passed a number that is divisible by both 2 and 5, launchOutput() returns 'Launch Rocks! (CRASH!!!!)'", function() {
+    let result = launchcode;
+    expect(result.launchOutput(10)).toEqual('Launch Rocks! (CRASH!!!!)');
+  });
+
+
 });
