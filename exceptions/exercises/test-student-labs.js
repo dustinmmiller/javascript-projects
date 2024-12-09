4eb30b25352351a7bde4118cd8aa5d1b420cc85b
dustinmmiller <d0nut01@hotmail.com>
2024-12-09 14:45:26 -0600

    added a try catch block to catch if someone didnt add a runLab property and throw an error if denominator was equal to 0

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,13 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let result;
+    try {
+      result = lab.runLab(3);
+    } catch(err) {
+      result = "Error Thrown";
+    }
+      console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
 
@@ -21,4 +26,26 @@ let studentLabs = [
   }
 ];
 
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
 gradeLabs(studentLabs);
