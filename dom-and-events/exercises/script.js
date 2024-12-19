5ca62f95f64fc1cf5e2129d40d5bcad40a0baf25
dustinmmiller <d0nut01@hotmail.com>
2024-12-19 12:33:39 -0600

    created events that changed the text for a webage and background color for the abort mission button as well as added a confirmation window if mission is aborted

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function (event) {
+        paragraph.innerHTML = "Houston, we have liftoff!";       
+    });
+
+    missionAbort.addEventListener("mouseover", function (event) {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function ( event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function (event) {
+        let answer = confirm("Are you sure you want to abort the mission?");
+        if (answer){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    });
 }
+
 
 window.addEventListener("load", init);
